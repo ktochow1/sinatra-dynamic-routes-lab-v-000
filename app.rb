d28bc19e3cc390013bc9869d208c289f7531fdc4
Kat Tochowicz <ktochow1@binghamton.edu>
2019-04-24 19:41:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -33,16 +33,7 @@ class App < Sinatra::Base
    number1 = params[:number1]
    number2 = params[:number2]
    
-    case params[:operation]
-    when 'add'
-      answer = (number1 + number2).to_s 
-      when 'subtract'
-        answer = (number1 - number2).to_s
-        when 'multiply'
-          answer = (number1 * number2).to_s 
-          when 'divide'
-            answer = (number1 / number2).to_s
-    end
+    
   end
 
 end
