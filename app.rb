54b6264a68d71cef61a4ec1b7123d7bda45158ac
Kat Tochowicz <ktochow1@binghamton.edu>
2019-04-24 19:42:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -32,7 +32,10 @@ class App < Sinatra::Base
   get '/:operation/:number1/:number2' do 
    number1 = params[:number1]
    number2 = params[:number2]
+   operation = params[:operation]
    
+   if 'add'
+     
     
   end
 
