b9661a8ae8cbfae9e21060178d3a613fa853ceb7
Kat Tochowicz <ktochow1@binghamton.edu>
2019-04-24 19:44:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -29,15 +29,22 @@ class App < Sinatra::Base
     "#{@word1} " + "#{@word2} " + "#{@word3} " + "#{@word4} " + "#{@word5}."
   end 
   
-  get '/:operation/:number1/:number2' do 
-   number1 = params[:number1].to_i
-   number2 = params[:number2].to_i
-   operation = params[:operation]
-   
-   if operation == 'add'
-     answer = number1 + number2
-   end
-    
+   get '/:operation/:number1/:number2' do
+    operation = params[:operation]
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
+    if operation == "add"
+      answer = number1 + number2
+    elsif operation == "subtract"
+      answer = number1 - number2
+    elsif operation == "multiply"
+      answer = number1 * number2
+    elsif operation == "divide"
+      answer = number1 / number2
+    else
+      answer = "Unable to perform this operation"
+    end
+    answer.to_s
   end
 
 end
